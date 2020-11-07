19872ea059d6ee8d1c1e76fa6445b666c060901f
Conrad Thiounn <conrad.thiounn@insee.fr>
2020-11-07 13:17:47 +0100

    empty dir and files

---
+++ day15.py
@@ -0,0 +1,22 @@
+# from collections import Counter
+# from collections import deque
+# import asyncio
+# import itertools
+# import json
+# import math
+# import re
+# import string
+# import sys
+# import unittest
+
+with open('data/my_input/15.in') as f:
+    lines = [  line.strip() for line in f]
+
+def part1(vlines):
+    return 0
+
+def part2(vlines):
+    return 0
+
+print(part1(lines))
+print(part2(lines))
