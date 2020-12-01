271404544b7a40f8d50d2224537cd2af3ed96c44
Conrad Thiounn <conrad.thiounn@insee.fr>
2020-12-01 06:10:53 +0100

    day1 Q&D

--- day1.py
+++ day1.py
@@ -13,9 +13,19 @@ with open('data/my_input/1.in') as f:
     lines = [  line.strip() for line in f]
 
 def part1(vlines):
+    for line in vlines:
+        for line2 in vlines:
+            if line!=line2 and int(line)+int(line2)==2020:
+                return int(line)*int(line2)
     return 0
 
 def part2(vlines):
+    
+    for line in vlines:
+        for line2 in vlines:
+            for line3 in vlines:
+                if line!=line2 and line!=line3 and line2!=line3 and  int(line3)+int(line)+int(line2)==2020:
+                    return int(line3)*int(line)*int(line2)
     return 0
 
 print(part1(lines))
