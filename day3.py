8231f54c182c28bb5ee189c9d67fa5442add752f
Conrad Thiounn <conrad.thiounn@insee.fr>
2020-12-03 08:42:31 +0100

    day3 Q&D

--- day3.py
+++ day3.py
@@ -1,22 +1,30 @@
-# from collections import Counter
-# from collections import deque
-# import asyncio
-# import itertools
-# import json
-# import math
-# import re
-# import string
-# import sys
-# import unittest
-
 with open('data/my_input/3.in') as f:
     lines = [  line.strip() for line in f]
 
 def part1(vlines):
-    return 0
+    x,y=0,0
+    count=0
+    for line in vlines:
+        if  line[x]=='#':
+            count+=1
+        x+=3
+        x= x%len(line)
+    return count
+
+def slope(lines,i,j):
+    count=0
+    x,y=0,0
+    while y<len(lines):
+        if  lines[y][x]=='#':
+            count+=1
+        x+=i
+        y+=j
+        if y <len(lines):
+            x= x%len(lines[y])
+    return count
 
 def part2(vlines):
-    return 0
+    return slope(vlines,1,1)* slope(vlines,3,1)* slope(vlines,5,1)* slope(vlines,7,1)* slope(vlines,1,2)
 
-print(part1(lines))
-print(part2(lines))
+print(part1(lines)) #164
+print(part2(lines)) #5007658656
