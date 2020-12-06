65cd8efb9a4a0636e898cc015b7cee19f8b8b04b
Conrad Thiounn <conrad.thiounn@insee.fr>
2020-12-06 06:52:28 +0100

    D6 q&d

--- day6.py
+++ day6.py
@@ -1,22 +1,33 @@
-# from collections import Counter
-# from collections import deque
-# import asyncio
-# import itertools
-# import json
-# import math
-# import re
-# import string
-# import sys
-# import unittest
-
 with open('data/my_input/6.in') as f:
     lines = [  line.strip() for line in f]
 
 def part1(vlines):
-    return 0
+    d=set()
+    counter=0
+    for line in vlines:
+        if len(line.strip())==0:
+            counter+=len(list(d))
+            d=set()
+        else:
+            for i in line:
+                d.add(i)
+    counter+=len(list(d))   
+        
+
+    return counter
 
 def part2(vlines):
-    return 0
+    counter=0
+    s=set("azertyuiopqsdfghjklmwxcvbn")
+    for line in vlines:
+        if len(line.strip())==0:
+            counter+=len(s)
+            s=set("azertyuiopqsdfghjklmwxcvbn")
+        else: 
+            s=s&set(line)
+    
+    counter+=len(s)
+    return counter
 
 print(part1(lines))
 print(part2(lines))
