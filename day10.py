160a2fec545f556873ae7c2d4ab140af88c1ee28
Conrad Thiounn <conrad.thiounn@insee.fr>
2020-12-10 07:04:16 +0100

    D10 Q&D

--- day10.py
+++ day10.py
@@ -1,16 +1,3 @@
-from collections import Counter
-# from collections import deque
-# import asyncio
-# import itertools
-# import json
-# import math
-import re
-# import string
-# import sys
-# import unittest
-import collections
-import math
-
 with open('data/my_input/10.in') as f:
     lines = [line.strip() for line in f]
 
@@ -23,15 +10,11 @@ def part1(vlines):
     chargingoutlet=0
     
     counter1=0
-    counter2=0
     counter3=0
     while li2:
         element=li2.pop(0)
         if abs(element-chargingoutlet)==1:
             counter1+=1
-            chargingoutlet=element
-        elif abs(element-chargingoutlet)==2:
-            counter2+=1
             chargingoutlet=element
         elif abs(element-chargingoutlet)==3:
             counter3+=1
@@ -63,5 +46,3 @@ def part2(vlines):
  
 print(part1(lines)) #1625
 print(part2(lines)) #3100448333024
-
-#3100448333024
