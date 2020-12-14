efcd0bb5f12171693f96235949da7d1246ea5a40
Conrad Thiounn <conrad.thiounn@insee.fr>
2020-12-14 07:11:08 +0100

    D14

--- day14.py
+++ day14.py
@@ -1,28 +1,79 @@
-# from collections import Counter
-# from collections import deque
-# import asyncio
-# import itertools
-# import json
-# import math
-# import re
-# import string
-# import sys
-# import unittest
+import re
 
 with open('data/my_input/14.in') as f:
     lines = [  line.strip() for line in f]
-    # dlines = { (x,y): cell for x,row in enumerate(lines) for y,cell in enumerate(row) }
 
-with open('data/test/13.test') as f2:
+with open('data/test/14.test') as f2:
     tests = [  test.strip() for test in f2]
 
+with open('data/test/14_2.test') as f3:
+    tests2 = [  test.strip() for test in f3]
+
+def calculate(number,mask):
+    bi="{0:b}".format(int(number))
+    bi=bi.zfill(36)
+    mys=""
+    for ind,i in enumerate(mask):
+        if "X" not in i:
+            mys+=i
+        else:
+            mys+=bi[ind]
+    return int(mys,2)
+
 def part1(vlines):
-    return 0
+    mask=''
+    num=lambda s: re.findall(r'\d+',s)
+    d=dict()
+    ss=0
+    for l in vlines:
+        if 'mask' in l:
+            mask=l.split(' ')[2]
+        else:
+            pos,number=num(l)
+            d[pos]=calculate(number,mask)
+    ss+=sum(d.values())
+    return ss
 
 def part2(vlines):
-    return 0
+    mask=''
+    num=lambda s: re.findall(r'\d+',s)
+    d=dict()
+    for l in vlines:
+        if 'mask' in l:
+            mask=l.split(' ')[2]
+        else:
+            pos,number=num(l)
+            if pos not in d:
+                write(d,calculate2(number,mask,"{0:b}".format(int(pos)).zfill(36)),int(number))
+            else:
+                write(d,calculate2(number,mask,"{0:b}".format(d[pos]).zfill(36)),int(number))
+    return sum([v for v in d.values()])
+
+def write(d,s,number):
+    num=s.count("X")
+    if num==0:
+        d[int(s,2)]=number
+    else:
+        xc=s
+        x=s
+        write(d,x.replace('X','0',1),number)
+        write(d,xc.replace('X','1',1),number)
+
+ 
+def calculate2(number,mask,number2):
+    bi="{0:b}".format(int(number))
+    bi=bi.zfill(36)
+    mys=""
+    for ind,i in enumerate(mask):
+        if "X"  in i:
+            mys+="X"
+        elif "1" in i:
+            mys+="1"
+        elif "0" in i:
+            mys+=number2[ind]
+    return mys
 
 print("test part1",part1(tests))
-print("output part1",part1(lines))
-print("test part2",part2(tests))
-print("output part2",part2(lines))
+print("output part1",part1(lines)) #10885823581193
+print("test part2",part2(tests2))
+print("output part2",part2(lines)) #3816594901962
