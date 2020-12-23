2dabccb700d92fdc795c25f9b667f5ffc8a6d8b1
Conrad Thiounn <conrad.thiounn@insee.fr>
2020-12-23 06:24:48 +0100

    D23 P1

--- day23.py
+++ day23.py
@@ -1,28 +1,32 @@
-# from collections import Counter
-# from collections import deque
-# import asyncio
-# import itertools
-# import json
-# import math
-# import re
-# import string
-# import sys
-# import unittest
 
 with open('data/my_input/23.in') as f:
-    lines = [  line.strip() for line in f]
-    # dlines = { (x,y): cell for x,row in enumerate(lines) for y,cell in enumerate(row) }
-
+    lines = int(f.read())
 with open('data/test/23.test') as f2:
-    tests = [  test.strip() for test in f2]
+    tests = int(f2.read())
 
-def part1(vlines):
+def part1(i,round):
+    ss=str(i)
+    l=len(ss)
+    for _ in range(round):
+        v=int(ss[0])
+        ref=v
+        nextv=ss[1:4]
+        remain=ss[4:]
+        while str(ref) not in remain:
+            ref-=1 
+            ref%=l
+            if ref==0:
+                ref=l
+        ii = remain.index(str(ref))
+        ss=remain[0:ii+1] + nextv + remain[ii+1:] + str(v)
+    ii = ss.index("1")
+    print(ss[ii+1:]+ss[0:ii])
     return 0
 
 def part2(vlines):
     return 0
 
-print("test part1",part1(tests))
-print("output part1",part1(lines))
-print("test part2",part2(tests))
-print("output part2",part2(lines))
+print("test part1",part1(tests,10))
+print("output part1",part1(lines,100))
+# print("test part2",part2(tests))
+# print("output part2",part2(lines))
