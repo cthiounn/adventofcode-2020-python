619e0dddc43dbb4a5fa55548dc70fdd06f5e8580
Conrad Thiounn <conrad.thiounn@insee.fr>
2020-12-23 08:58:12 +0100

    D23 P2

--- day23.py
+++ day23.py
@@ -20,13 +20,59 @@ def part1(i,round):
         ii = remain.index(str(ref))
         ss=remain[0:ii+1] + nextv + remain[ii+1:] + str(v)
     ii = ss.index("1")
-    print(ss[ii+1:]+ss[0:ii])
-    return 0
+    return ss[ii+1:]+ss[0:ii]
 
-def part2(vlines):
-    return 0
+class N:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+def part2(nums,rou):
+    sNode=dict()
+    prev=None
+    for j in str(nums):
+        p=N(int(j))
+        sNode[int(j)]=p
+        if prev:
+            prev.next=p
+        prev=p
+    a=len(str(nums))
+
+    for m in range(a+1,1000000+1):
+        p=N(int(m))
+        sNode[int(m)]=p
+        if prev:
+            prev.next=p
+        prev=p
+        
+    for j in str(nums):
+        first=sNode[int(j)]
+        if prev:
+            prev.next=first
+        break
+    
+    for i in range(rou):
+        second=first.next
+        third=second.next
+        q4=third.next
+        first.next=q4.next
+        ind=first.val
+        forbidden=(first.val,second.val,third.val,q4.val)
+        while ind  in forbidden:
+            ind -= 1
+            if ind == 0:
+                ind = 1000000
+        node_to_modified= sNode[ind]
+        node_to_modified2=node_to_modified.next
+        node_to_modified.next=second
+        q4.next=node_to_modified2
+        first=first.next
+    s1=sNode[1].next
+    s2=s1.next
+    return s1.val*s2.val
 
 print("test part1",part1(tests,10))
-print("output part1",part1(lines,100))
-# print("test part2",part2(tests))
-# print("output part2",part2(lines))
+print("test part1",part1(tests,100))
+print("output part1",part1(lines,100)) #39564287
+print("test part2",part2(tests,10000000)) 
+print("output part2",part2(lines,10000000)) #404431096944
