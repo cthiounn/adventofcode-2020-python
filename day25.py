6847cf47d4e695b5a86f6a2b4f0bedcf361f7f02
Conrad Thiounn <conrad.thiounn@insee.fr>
2020-12-25 08:57:21 +0100

    D25

--- day25.py
+++ day25.py
@@ -1,28 +1,31 @@
-# from collections import Counter
-# from collections import deque
-# import asyncio
-# import itertools
-# import json
-# import math
-# import re
-# import string
-# import sys
-# import unittest
+
 
 with open('data/my_input/25.in') as f:
-    lines = [  line.strip() for line in f]
-    # dlines = { (x,y): cell for x,row in enumerate(lines) for y,cell in enumerate(row) }
-
+    lines = [  int(line.strip()) for line in f]
+ 
 with open('data/test/25.test') as f2:
-    tests = [  test.strip() for test in f2]
+    tests = [  int(test.strip()) for test in f2]
 
+def findloopnum_or_calculate(cpk,subjectnumber,calculate):
+    loopsize=1
+    initvalue=1
+    while True:
+        if calculate:
+            test =(initvalue* cpk)%20201227
+        else:
+            test =(initvalue* subjectnumber)%20201227
+        if not calculate and cpk==test :
+            return loopsize
+        elif calculate and loopsize==subjectnumber:
+            return test
+        initvalue=test
+        loopsize+=1
+    
+    
 def part1(vlines):
-    return 0
-
-def part2(vlines):
-    return 0
-
-print("test part1",part1(tests))
-print("output part1",part1(lines))
-print("test part2",part2(tests))
-print("output part2",part2(lines))
+    cpk,dpk= vlines
+    a,b= findloopnum_or_calculate(cpk,7,False),findloopnum_or_calculate(dpk,7,False)
+    return findloopnum_or_calculate(cpk,b,True)
+ 
+print("test part1",part1(tests)) #14897079
+print("output part1",part1(lines)) #12929
